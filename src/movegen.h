95fc73c634b830058c9bc9127ddb5c81c7725265
pauxav06 <paul.heibig@epfl.ch>
2025-07-24 18:34:54 +0200

    jecommit

--- src/movegen.h
+++ src/movegen.h
@@ -8,22 +8,22 @@
 
 
 
-typedef enum movegen_type_t : int8_t
+ enum movegen_type_t : int8_t
 {
     QUIET,
     CAPTURE,
     ESCAPE,
     PSEUDO_LEGAL,
     LEGAL
-} movegen_type_t;
+} ;
 
 
-typedef enum move_type_t {
+ enum move_type_t {
     NORMAL,
     PROMOTION  = 1 << 14,
     EN_PASSANT = 2 << 14,
     CASTLING   = 3 << 14
-} move_type_t;
+} ;
 
 // a move is encoded as an 16bit unsigned int
 // 0-5 bit : to square (square 0 to 63)
@@ -33,13 +33,14 @@ typedef enum move_type_t {
 class move_t
 {
   public:
-    constexpr explicit move_t(std::uint16_t d) : data(d) {}
+    constexpr explicit move_t(const std::uint16_t d) : data(d) {}
 
-    constexpr move_t(square_t from, square_t to) : data((from << 6) + to) {}
+    constexpr move_t(const square_t from, const square_t to) : data((from << 6) + to) {}
 
 	// to build a move if you already know the type of move
     template<move_type_t T>
-    static constexpr move_t make(square_t from, square_t to, piece_type_t pt = KNIGHT) {
+    static constexpr move_t make(const square_t from, const square_t to,
+                                 const piece_type_t pt = KNIGHT) {
         return move_t(T + ((pt - KNIGHT) << 12) + (from << 6) + to);
     }
 
@@ -59,22 +60,17 @@ class move_t
 
 	constexpr square_t from_sq() const {
         assert(is_ok());
-        return square_t((data >> 6) & 0x3F);
+        return static_cast<square_t>((data >> 6) & 0x3F);
     }
 
     constexpr square_t to_sq() const {
         assert(is_ok());
-        return square_t(data & 0x3F);
+        return static_cast<square_t>(data & 0x3F);
     }
 
-	constexpr move_type_t type_of() const { return move_type_t(data & (3 << 14)); }
+	constexpr move_type_t type_of() const { return static_cast<move_type_t>(data & (3 << 14)); }
 
-    constexpr pice_type_t promotion_type() const { return piece_type_t(((data >> 12) & 3) + KNIGHT); }
-
-
-    move_t(square_t from, square_t to);
-    square_t from();
-    square_t to();
+    constexpr piece_type_t promotion_type() const { return static_cast<piece_type_t>((data >> 12 & 3) + KNIGHT); }
 
   private:
     std::uint16_t data;
@@ -83,7 +79,7 @@ class move_t
 class move_list_t
 {
   public:
-    static const size_t max_moves = 256;
+    static constexpr size_t max_moves = 256;
     void                add(move_t m);
 };
 
@@ -100,36 +96,35 @@ void gen_pawn_moves(position_t* pos, state_t* state, move_list_t* list)
     const bitboard_t     ep_bb =
         (state->ep_square == NO_SQUARE ? bb::empty : bb::sq_mask(state->ep_square));
     // straight one
-    if constexpr (mt != CAPTURE)
     {
         bitboard_t single_push = bb::shift<up>(pawns & ~bb_promotion_rank) & available;
         bitboard_t double_push = bb::shift<up>(single_push & bb_third_rank) & available;
         while (single_push)
         {
-            square_t sq = square_t(pop_lsb(single_push));
-            list->add(move_t(sq - up, sq));
+            const square_t sq = static_cast<square_t>(pop_lsb(single_push));
+            list->add(move_t(static_cast<square_t>(sq - up), sq));
         }
         while (double_push)
         {
-            square_t sq = square_t(pop_lsb(double_push));
-            list->add(move_t(sq - 2 * up, sq));
+            const square_t sq = static_cast<square_t>(pop_lsb(double_push));
+            list->add(move_t(static_cast<square_t>(sq - 2 * up), sq));
         }
     }
     bitboard_t promotion = pawns & bb_promotion_rank;
     if (promotion)
     {
     }
-    if constexpr (mt & CAPTURE)
+    //capture
     {
         bitboard_t remaining  = pawns;
         bitboard_t capturable = enemy | ep_bb;
         while (remaining)
         {
-            square_t   from    = square_t(pop_lsb(remaining));
+            const square_t   from    = static_cast<square_t>(pop_lsb(remaining));
             bitboard_t attacks = (bb::attacks<PAWN>(from, enemy, c)) & capturable;
             while (attacks)
             {
-                square_t to = square_t(pop_lsb(attacks));
+                const square_t to = static_cast<square_t>(pop_lsb(attacks));
                 list->add(move_t(from, to));
             }
         }
