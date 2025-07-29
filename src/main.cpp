4348829939f6afc6007093d9d9eac44b499eb7e5
pauxav06 <paul.heibig@epfl.ch>
2025-07-29 12:26:16 +0200

    https://wiki.sharewiz.net/doku.php?id=chess:programming:perft all perft tests pass, adding it to cmake tests

--- src/main.cpp
+++ src/main.cpp
@@ -69,6 +69,16 @@ int main()
     //pos.from_fen("n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1");
     //pos.do_move(move_t::make<NORMAL>(D7, C6));
 //pos.do_move(move_t::make<PROMOTION>(B7, A8, ROOK));
+    pos.from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 ");
+    pos.from_fen("n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1 ");
+    pos.from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -");
+    pos.from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -");
+    pos.from_fen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
+    pos.from_fen("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1");
+    pos.from_fen("8/3K4/2p5/p2b2r1/5k2/8/8/1q6 b - 1 67");
+    pos.from_fen("8/7p/p5pb/4k3/P1pPn3/8/P5PP/1rB2RK1 b - d3 0 28");
+    pos.from_fen("rnbqkb1r/ppppp1pp/7n/4Pp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 3");
+    pos.from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -");
 
 
 
